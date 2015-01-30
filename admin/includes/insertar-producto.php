8aa3f41fbb45f3662dae620ed00cfddf80335e71
Moyo2100 <moises.rojas@leon.uia.mx>
2015-01-30 10:28:32 -0600

    Grupo H
    Proyecto con la opción de seleccionar fecha y subir una imagen

--- admin/includes/insertar-producto.php
+++ admin/includes/insertar-producto.php
@@ -5,8 +5,20 @@ $nombre = $_POST['nombre_producto'];
 $descripcion= $_POST['descripcion_producto'];
 $precio = $_POST['precio'];
 $categoria = $_POST['categoria'];
+$fecha=$_POST['fecha_lanzamiento'];
+
+$nombre_archivo = $_FILES['imagen_producto']['name'];
+$tipo_archivo = $_FILES['imagen_producto']['type'];
+$tmp_archivo = $_FILES['imagen_producto']['tmp_name'];
+$nombre_final_archivo = date("His") . "-" . $nombre_archivo;
+
+$directorio_archivos = "../img/Portadas/";
+
+move_uploaded_file($tmp_archivo, $directorio_archivos . $nombre_final_archivo);
+
+
 $consulta_insertar="INSERT INTO Productos
-(clave_producto,nombre_producto,descripcion_producto,precio,categoria)
-VALUES ('$clave','$nombre','$descripcion','$precio','$categoria')";
+(clave_producto,nombre_producto,descripcion_producto,imagen_producto,precio,categoria,fecha_lanzamiento)
+VALUES ('$clave','$nombre','$descripcion','$nombre_final_archivo','$precio','$categoria','$fecha')";
 mysqli_query($conexion,$consulta_insertar);
 ?>
