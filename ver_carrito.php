45a6d5d7bd7a9ca42220ad3e8fae8b4de56f2b6a
Moyo2100 <moises.rojas@leon.uia.mx>
2015-04-28 10:09:54 -0500

    GRUPO H
    Proyecto con carrito conectado a PAYPAL

--- ver_carrito.php
+++ ver_carrito.php
@@ -47,7 +47,48 @@ $titulo = "Productos Agregados al carrito";
 		<div><p> Total de artículos: <?php echo count($carro); ?></p></div>
 		<div><p> Total a pagar: $<?php echo number_format($suma,2); ?></p></div>
 		<div><a href="index.php">Continuar Comprando</a></div>
-		<div><a href="finalizar_compra.php">Finalizar Compra</a></div>
+		
+		
+			<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="POST">
+			
+			<!--COSTO DEL ENVÍO-->
+			<input type="hidden" name="shipping" value="0">
+			<!--DEFINE LA LEYENDA PARA REGRESAR AL SITIO-->
+			<input type="hidden" name="cbt" value="Presione aquí para volver al sitio www.MiTienda.com">
+			<!-- INDICAMOS EL METODO POR EL CUAL ENVIAMOS LA INFORMACIÓN 1 = GET y 2 = POST -->
+			<input type="hidden" name="rm" value="2">
+			<!--DEFINE EL NOMBRE DEL SITIO AL CUAL SE VA A REALIZAR EL PAGO-->
+			<input type="hidden" name="bn" value="Mi Tienda de Peliculas" >
+			<!--LA CUENTA DE PAYPAL QUE VA A RECIBIR EL PAGO-->
+			<input type="hidden" name="business" value="moises.rojas@leon.uia.mx">
+			<!--INDICAMOS EL CONCEPTO DEL PAGO POR UN PRODUCTO O SERVICIO-->
+			<input type="hidden" name="item_name" value="Peliculas" >
+			<!--INDICAMOS EL TOTAL DE PRODUCTOS EN EL CARRITO-->
+			<input type="hidden" name="item_number" value="<?php echo count($carro); ?>">
+			<!--INDICAMOS EL TOTAL A PAGAR -->
+			<input type="hidden" name="amount" value="<?php echo $suma; ?>">
+			<!--INDICAMOS LAS VARIABLES PERSONALIZADAS CON LAS QUE VAMOS A TRABAJAR-->
+			<input type="hidden" name"custom" value="<?php echo $_SESSION['nombre_usuario']; ?>">
+			<!--INDICAMOS LA DIVISA CON LA QUE VAMOS A COBRAR-->
+			<input type="hidden" name="currency_code" value="MXN">
+			<!--INDICAMOS LA RUTA ABSOLUTA DEL LOGOTIPO O IMAGEN A COLOCAR EN EL PAGO DE PAYPAL-->
+			<input type="hidden" name="image_url" value="http://leon.uia.mx/images/escudo.gif">
+			<!--INDICAMOS LA RUTA ABSOLUTA DE LA PÁGINA A DONDE SE VA A ENVIAR AL USUARIO SI EL PAGO ES EXITOSO-->
+			<input type="hidden" name="return" value="http://www.MiTienda.com/ipn_success.php">
+			<!--INDICAMOS LA RUTA ABSOLUTA DE LA PÁGINA A DONDE SE VA A ENVIAR AL USUARIO SI EL PAGO NO ES EXITOSO-->
+			<input type="hidden" name="cancel_return" value="http://www.MiTienda.com/ipn_error.php">
+			<!--INDICAMOS SI EL USUARIO PUEDE O NO DEJAR UN COMENTARIO AL MOMENTO DE REALIZAR UN PAGO 1 = SI y 0 = NO-->
+			<input type="hidden" name="no_vote" value="0">
+			<!--INDICAMOS SI EL USUARIO PUEDE O NO SELECCIONAR UN TIPO DE ENVIO PAGO 1 = SI y 0 = NO-->
+			<input type="hidden" name="no_shipping" value="0">
+			
+			<!--VALOR QUE VALIDA QUE LA INFORMACIÓN SE ENVIÓ A TRAVÉS DE UN FORMULARIO-->
+			<input type="hidden" name="cmd" value="_xclick">
+			
+			<input type="submit" value="FINALIZAR COMPRA">
+			</form>
+		
+		
 		<?php } else { ?>
 			<p>No hay productos seleccionados -
 			<a href="index.php">Ver Productos</a>
